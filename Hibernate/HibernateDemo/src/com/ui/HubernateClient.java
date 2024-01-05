154f366ee5b2d0889d2cc73e88b51c8215fd9ec9
Shalini Mittal <shalini06.mittal@gmail.com>
2024-01-05 21:22:20 +0530

    hibernate delete and update

--- Hibernate/HibernateDemo/src/com/ui/HubernateClient.java
+++ Hibernate/HibernateDemo/src/com/ui/HubernateClient.java
@@ -9,27 +9,47 @@ import com.entity.Employee;
 
 public class HubernateClient {
 
+	private static SessionFactory sessionFactory ;
 	public static void main(String[] args) {
 		
 		// load the xml file and do all the pre-initialization to connect with the database
 		Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
-		SessionFactory sessionFactory = configuration.buildSessionFactory();
+		sessionFactory = configuration.buildSessionFactory();
 		
 		Employee e1 = new Employee(3, "Vedant", "Mumbai", "1212121212");
 		Employee e2 = new Employee(2, "Ajit", "Mumbai", "6726567576");
 		
+		e2.setEname("Ajit Mittal");
+		updateEmployee(e2);
+		deleteEmployee(e1);
+		
+	}
+	public static void addEmployee(Employee employee)
+	{
 		Session session = sessionFactory.openSession();
 		// DML -> insert , update and delete
 		Transaction tx = session.beginTransaction();
-		session.save(e1); // insert into emp (city, countryCode, empname, phone, id) values (?, ?, ?, ?, ?) 
-		session.save(e2);
+		session.save(employee);
 		tx.commit();
 		session.close();
-		
-		//Scanner
-		// String name = sc.next();
-		
-		
+	}
+	public static void deleteEmployee(Employee employee)
+	{
+		Session session = sessionFactory.openSession();
+		// DML -> insert , update and delete
+		Transaction tx = session.beginTransaction();
+		session.delete(employee);
+		tx.commit();
+		session.close();
+	}
+	public static void updateEmployee(Employee employee)
+	{
+		Session session = sessionFactory.openSession();
+		// DML -> insert , update and delete
+		Transaction tx = session.beginTransaction();
+		session.update(employee);
+		tx.commit();
+		session.close();
 	}
 
 }
